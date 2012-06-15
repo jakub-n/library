0ac9b23fa646ee599520858243b5d2c05b333318
veru <veru@localhost>
2012-06-15 10:51:25 +0200

    Základní verze správy čtenářů, vyhledávání čtenářů

--- Library/src/main/java/cz/muni/fi/pv243/library/ejb/BookLoanManager.java
+++ Library/src/main/java/cz/muni/fi/pv243/library/ejb/BookLoanManager.java
@@ -2,14 +2,17 @@ package cz.muni.fi.pv243.library.ejb;
 
 import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.jboss.solder.logging.Logger;
 
 import cz.muni.fi.pv243.library.entity.BookLoan;
+import cz.muni.fi.pv243.library.entity.Reader;
 import cz.muni.fi.pv243.library.resource.LibraryDatabase;
 
 @Stateless
@@ -62,6 +65,37 @@ public class BookLoanManager {
 		return q.getResultList();
 	}
 	
+
+	/**
+	 * Returns count of book loans for reader.
+	 * 
+	 * @param reader
+	 * @return count of book loans
+	 */
+	@RolesAllowed({ "MANAGER", "LIBRARIAN" })
+	public int count(Reader reader) {
+		Query query = em
+				.createQuery("SELECT count(a) FROM BookLoan a WHERE a.reader.username='"
+						+ reader.getUsername() + "'");
+		return ((Long) query.getSingleResult()).intValue();
+	}
+
+	/**
+	 * Returns book loans by given range.
+	 * 
+	 * @param range
+	 * @return book loans by given range
+	 */
+	@RolesAllowed({ "MANAGER", "LIBRARIAN" })
+	public List<BookLoan> findRange(int[] range, Reader reader) {
+		Query query = em
+				.createQuery("SELECT a FROM BookLoan a WHERE a.reader.username='"
+						+ reader.getUsername() + "'");
+
+		query.setFirstResult(range[0]);
+		query.setMaxResults(range[1] - range[0]);
+		return query.getResultList();
+	}
 	
 
 }
--- Library/src/main/java/cz/muni/fi/pv243/library/web/controller/ReaderController.java
+++ Library/src/main/java/cz/muni/fi/pv243/library/web/controller/ReaderController.java
@@ -6,6 +6,8 @@ import java.util.Set;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import javax.inject.Inject;
 
 import org.jboss.solder.logging.Logger;
@@ -14,6 +16,7 @@ import cz.muni.fi.pv243.library.ejb.UserManager;
 import cz.muni.fi.pv243.library.entity.Reader;
 import cz.muni.fi.pv243.library.entity.Role;
 import cz.muni.fi.pv243.library.entity.User;
+import cz.muni.fi.pv243.library.web.util.AbstractPaginationHelper;
 import cz.muni.fi.pv243.library.web.util.JsfUtil;
 
 
@@ -24,7 +27,9 @@ public class ReaderController implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Inject
-	ReaderManager readerManager;
+	private ReaderManager readerManager;
+	private AbstractPaginationHelper pagination;
+	private DataModel<Reader> items = null;
 
 	@Inject
 	UserManager userManager;
@@ -33,6 +38,8 @@ public class ReaderController implements Serializable {
 	private Logger log;
 
 	private Reader current;
+	private int selectedItemIndex;
+	private String searchText = null;
 
 	/**
 	 * Constructor.
@@ -92,4 +99,192 @@ public class ReaderController implements Serializable {
 		return (userManager.getUserByUsername(user.getUsername()) == null) ? false
 				: true;
 	}
+	
+
+	/**
+	 * Returns data model of readers.
+	 * 
+	 * @return data model of readers
+	 */
+	public DataModel<Reader> getItems() {
+		if (items == null) {
+			items = getPagination().createPageDataModel();
+		}
+		return items;
+	}
+
+	/**
+	 * Method for pagination
+	 * 
+	 * @return AbstractPaginationHelper
+	 */
+	public AbstractPaginationHelper getPagination() {
+		if (pagination == null) {
+			pagination = new AbstractPaginationHelper(5) {
+				@Override
+				public int getItemsCount() {
+					return readerManager.count();
+				}
+
+				@Override
+				public DataModel<Reader> createPageDataModel() {
+					return new ListDataModel<Reader>(readerManager.findRange(
+							new int[] { getPageFirstItem(),
+									getPageFirstItem() + getPageSize() },
+							searchText));
+				}
+			};
+		}
+
+		return pagination;
+	}
+
+	/**
+	 * Method which loads selected reader and navigates to reader detail.
+	 * 
+	 * @return reader detail view
+	 */
+	public String prepareView() {
+		current = (Reader) getItems().getRowData();
+		selectedItemIndex = pagination.getPageFirstItem()
+				+ getItems().getRowIndex();
+
+		return "/librarian/readerDetail";
+	}
+
+	/**
+	 * Performs delete of current reader and navigates to list of readers.
+	 * 
+	 * @return reader list view
+	 */
+	public String destroyAndView() {
+		performDestroy();
+		recreateModel();
+
+		return "/librarian/readersList";
+
+	}
+
+	/**
+	 * Deletes current reader.
+	 */
+	private void performDestroy() {
+		try {
+			readerManager.delete(current);
+			JsfUtil.addSuccessMessage("Čtenář byl úspěšně smazán.");
+		} catch (Exception e) {
+			JsfUtil.addErrorMessage("Při mazání čtenáře nastal problém.");
+		}
+	}
+
+	/**
+	 * Recreates the model (list).
+	 */
+	private void recreateModel() {
+		items = null;
+	}
+
+	/**
+	 * Method which loads selected reader and navigates to reader edit.
+	 * 
+	 * @return reader edit view
+	 */
+	public String prepareEdit() {
+		current = (Reader) getItems().getRowData();
+		selectedItemIndex = pagination.getPageFirstItem()
+				+ getItems().getRowIndex();
+
+		return "/librarian/editReader";
+	}
+
+	/**
+	 * Updates current reader.
+	 * 
+	 * @return navigation to updated reader view
+	 */
+	public String update() {
+		try {
+			readerManager.update(current);
+			JsfUtil.addSuccessMessage("Čtenář byl úspěšně upraven.");
+
+			return "/librarian/readerDetail";
+		} catch (Exception e) {
+			JsfUtil.addErrorMessage("Při úpravě čtenáře došlo k chybě.");
+
+			return null;
+		}
+	}
+
+	/**
+	 * Navigates to next page of pagination.
+	 * 
+	 * @return reader list view
+	 */
+	public String next() {
+		getPagination().nextPage();
+		recreateModel();
+
+		return "/librarian/readersManagement";
+	}
+
+	/**
+	 * Navigates to previous page of pagination.
+	 * 
+	 * @return reader list view
+	 */
+	public String previous() {
+		getPagination().previousPage();
+		recreateModel();
+
+		return "/librarian/readersManagement";
+	}
+
+	public String getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
+
+	
+	/**
+	 * Cleans the search text and navigates to readers' management page.
+	 * 
+	 * @return reader list view
+	 */
+	public String cleanAndShowManagement() {
+		searchText = null;
+		recreateModel();
+		return "/librarian/readersManagement";
+	}
+
+	/**
+	 * Cleans the search text and recreates model.
+	 */
+	public void cleanSearch() {
+		searchText = null;
+		recreateModel();
+	}
+	
+	/**
+	 * Cleans the current reader and navigates to registration page.
+	 * 
+	 * @return reader registration page
+	 */
+	public String cleanAndShowRegistration() {
+		current = new Reader();
+		return "/librarian/createReader";
+	}
+	
+
+	public Reader getCurrent() {
+		return current;
+	}
+
+	public void setCurrent(Reader current) {
+		this.current = current;
+	}
+
+	
 }
