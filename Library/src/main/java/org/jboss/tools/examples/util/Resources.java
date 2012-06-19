a65e7df5e3ac0e1ec2fa6d2439d5aaa18cfa6af2
czerny <czerny.jakub@gmail.com>
2012-06-19 04:21:59 +0200

    revert commitu accf622e4c111230 - 'Arquillian - create book test (For green bar disable security and logging first.)'; zpusoboval mi chyby, kdy weld hlasil neuspokojeni pozadavku pro injektnuti entity manageru

--- Library/src/main/java/org/jboss/tools/examples/util/Resources.java
+++ Library/src/main/java/org/jboss/tools/examples/util/Resources.java
@@ -1,13 +1,11 @@
-package cz.fi.muni.pv243.library.test;
+package org.jboss.tools.examples.util;
 
-
+import java.util.logging.Logger;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
-import org.jboss.solder.logging.Logger;
 
 /**
  * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
